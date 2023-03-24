da038834429b5404e3206441d460d3e5fcdff5e8
z4ng1ew <123883614+z4ng1ew@users.noreply.github.com>
2023-03-24 13:42:32 +0300

    Add files via upload

---
+++ regist.js
@@ -0,0 +1,6 @@
+const regist = document.querySelector('.button');
+const blockEl = document.querySelector('.block');
+regist.addEventListener('click', function () {
+    blockEl.classList.toggle('visible')
+
+});
